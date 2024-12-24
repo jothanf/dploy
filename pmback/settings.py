3b6a330f277bb535ad00fc586d2d3fce66ab8c3b
Jothanf <jothanferney@gmail.com>
2024-12-24 10:28:14 -0500

    Update ALLOWED_HOSTS in settings.py to include production URLs

--- pmback/settings.py
+++ pmback/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-siy5knb5zsdk*fl%tu8#kz+f@!$dhkvh8_=*pb=8yys^7rsl0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://*', 'https://web-production-67a2.up.railway.app/']
 
 
 # Application definition
