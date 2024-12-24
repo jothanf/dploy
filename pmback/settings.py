8851d58f31e9278f273baf0b7bc19e1bac9d3c88
Jothanf <jothanferney@gmail.com>
2024-12-24 12:56:04 -0500

    Enhance static file handling and update .gitignore
    - Added 'whitenoise.middleware.WhiteNoiseMiddleware' to MIDDLEWARE for improved static file serving in production.
    - Updated STATIC_URL to '/static/' for consistency with deployment practices.
    - Configured STATICFILES_STORAGE to use 'whitenoise.storage.CompressedManifestStaticFilesStorage' for optimized static file management.
    - Included '.env' and 'staticfiles' in .gitignore to prevent sensitive data exposure and unnecessary files in version control.

--- pmback/settings.py
+++ pmback/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,10 +133,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración adicional para whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configuración para servir archivos estáticos en producción
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
