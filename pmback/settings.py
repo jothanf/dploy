04947507ef7cf07cfa39cd89ae2c2a8320592cfb
Jothanf <jothanferney@gmail.com>
2024-12-24 10:43:11 -0500

    Enhance settings.py to include CORS and REST framework support
    - Added 'corsheaders' and 'rest_framework' to INSTALLED_APPS for improved API functionality and cross-origin resource sharing.

--- pmback/settings.py
+++ pmback/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'corsheaders',
+    'rest_framework',
     'dashboard',
 ]
 
